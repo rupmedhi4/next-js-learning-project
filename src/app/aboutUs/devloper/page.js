9f42bc9ea3f209f66a734932b8e981a295172bd1
Rup medhi <rupmmedhi@gmail.com>
2025-06-20 19:43:16 +0530

    Making Nested Routes

---
+++ src/app/aboutUs/devloper/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>
+            <h1>Developer Info</h1>
+            <p>Name: Rup medhi </p>
+        </div>
+    )
+}
